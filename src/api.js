1d3e95ed3afe8960b080d44939ad819834ab218a
Kyle Jensen <kylenjensen19@gmail.com>
2019-11-04 10:38:11 -0800

    finished exercise 4

--- src/api.js
+++ src/api.js
@@ -1,7 +1,19 @@
-const getItems = function () {
-  return Promise.resolve('A successful response!');
-};
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/kyle';
+const getItems = function () {
+ 
+  return fetch(`${BASE_URL}/items`);
+ 
+};
+let createItem = function(name){
+  let newItemString =JSON.stringify({name});
+  return fetch(`${BASE_URL}/items`,{
+    method: 'POST',
+    headers:{'Content-Type': 'application/json'},
+    body:newItemString
+  });
+};
+ 
 export default {
-  getItems
+  getItems,
+  createItem
 };
--- src/index.js
+++ src/index.js
@@ -5,12 +5,23 @@ import './index.css';
 
 
 import shoppingList from './shopping-list';
-api.getItems()
-  .then(res => console.log(res));
+// api.getItems()
+//   .then(res => res.json());
 
-console.log(api.BASE_URL);
+
+// console.log(api.BASE_URL);
+
 const main = function () {
-  
+  api.createItem('pears')
+    .then(res => res.json())
+    .then((newItem) => {
+      console.log("newItem:",newItem);
+      return api.getItems();
+    })
+    .then(res => res.json())
+    .then((items) => {
+      console.log(items);
+    });  
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
