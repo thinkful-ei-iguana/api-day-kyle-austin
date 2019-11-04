71766a1bd25bf2187c74708780cb968b1b824f95
Kyle Jensen <kylenjensen19@gmail.com>
2019-11-04 08:40:15 -0800

    did the second exercise

--- src/api.js
+++ src/api.js
@@ -1,9 +1,7 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
-
 const getItems = function () {
   return Promise.resolve('A successful response!');
 };
-
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/kyle';
 export default {
   getItems
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import $ from 'jquery';
-
+import api from './api';
 import 'normalize.css';
 import './index.css';
 
-import shoppingList from './shopping-list';
 
+import shoppingList from './shopping-list';
+api.getItems()
+  .then(res => console.log(res));
+
+console.log(api.BASE_URL);
 const main = function () {
+  
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
